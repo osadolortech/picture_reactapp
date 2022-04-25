8aa0855542f580aedd4d7c1698269123cec2cc36
osadolortech <brightosadolor8@gmail.com>
2022-04-25 10:49:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './App.css'
+import Calm from './animals/calm_cat.jpg'
+import Cat from './animals/crazy_cat.jpg'
+import Dog from './animals/crazy_dog.jpg'
+import Piglet from './animals/piglet.jpg'
+import Sloth from './animals/sloth.jpg'
+import Squre from './animals/squre.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () =>{
+
+    const list = document.querySelector('.image')
+    const [item,setitems] = useState([])
+
+    // const [images, setimages] = useState(Array.from(list))
+
+    // const fetchmoreimage = () =>{
+    //     setimages(images.concat(Array.from(list)))
+    // }
+    const newpage = () => {
+        const page = document.createElement('div')
+        page.className = 'image'
+        page.innerHTML = list.innerHTML
+
+        setitems(item.append(page))
+    }
+    
+        window.onscroll = ()=>{
+            if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+                
+                newpage()
+            }
+        }
+
+    
+
+    return (
+        <div className="project-container">
+            <header className="header">
+
+                <div className="nav_container"> 
+                    <input type="checkbox" className="toggle_nav" for="toggle"></input> 
+                    <div className="hambuger"></div>
+
+                    <nav className="nav">
+                    
+                        <a href="#Home">Home</a>
+                        <a href="#Gallary">Gallary</a>
+                        <a href="#Contacts">Contacts</a>
+                        <a href="#About">About</a>
+                    
+                    </nav>
+                </div>
+            </header>
+            <div className="scroll">
+                <div className="image">
+                    <img src={Calm}/>
+                </div>
+                <div className="image">
+                    <img src={Cat}/>
+                </div>
+                <div className="image">
+                    <img src={Dog}/>
+                </div>
+                <div className="image">
+                    <img src={Piglet}/>
+                </div>
+                <div className="image">
+                    <img src={Sloth}/>
+                </div>
+                <div className="image">
+                    <img src={Squre}/>
+                </div>
+
+            </div>
+            <div className="scroll">
+                {/* <InfiniteScroll
+                dataLength={images.length}
+                next={fetchmoreimage}
+                loader={<a>Loading...</a>}
+                scrollableTarget="scroll"
+                >
+                    {images.map((i,index)=>(
+                       <div key={index}>
+                           div - #{index}
+                       </div> 
+                    ))}
+                </InfiniteScroll> */}
+            </div>
+           
+        </div>
+    )
 }
-
-export default App;
+export default App
